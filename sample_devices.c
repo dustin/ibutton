1a50ad76c4eb307d59231359c2d8e922ee4d3e27
Dustin Sallings <dustin@spy.net>
1999-12-09 10:04:00 +0000

    Working logger.
    Author: dustin
    Date: 1999/12/09 02:04:00
    
    
    imported from: dustin@spy.net--projects/ibutton--fromcvs--0.1--patch-14

--- sample_devices.c
+++ sample_devices.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1999  Dustin Sallings <dustin@spy.net>
  *
- * $Id: sample_devices.c,v 1.1 1999/12/08 09:54:37 dustin Exp $
+ * $Id: sample_devices.c,v 1.2 1999/12/09 02:04:01 dustin Exp $
  */
 
 #include <stdio.h>
@@ -9,24 +9,26 @@
 #include <string.h>
 #include <unistd.h>
 #include <assert.h>
+#include <time.h>
+#include <sys/time.h>
 #include <mlan.h>
 
 /* NOTE:  The initialization may be called more than once */
-MLan *
+static MLan *
 init(char *port)
 {
 	return(mlan_init(port, PARMSET_9600));
 }
 
 /* Uninitializer */
-void
+static void
 teardown(MLan *mlan)
 {
 	mlan->destroy(mlan);
 }
 
 /* Record the current sample into a file unique to the serial number */
-void
+static void
 record_cur(const char *dir, const char *serial, const char *value)
 {
 	char	fn[8192];
@@ -45,22 +47,57 @@ record_cur(const char *dir, const char *serial, const char *value)
 	fclose(f);
 }
 
+/* Get the time for the record. */
 static char*
 get_time_str()
 {
-	/* Clock generation code goes here */
+	struct timeval tv;
+	struct tm *tp;
+	struct timezone tz;
+	static char str[50];
+	double seconds;
+
+	/* Get some better precisioned time */
+	gettimeofday(&tv, &tz);
+
+	tp = localtime(&tv.tv_sec);
+	seconds = tp->tm_sec+(double)tv.tv_usec/1000000;
+	sprintf(str, "%.4d/%.2d/%.2d %.2d:%.2d:%s%.6f",
+		tp->tm_year + 1900,
+		tp->tm_mon + 1,
+		tp->tm_mday,
+		tp->tm_hour,
+		tp->tm_min,
+		(seconds <10 ? "0" : ""),
+		seconds);
+	return(str);
+}
+
+/* Get the serial number as a string */
+static char*
+get_serial(uchar *serial)
+{
+	static char r[(MLAN_SERIAL_SIZE * 2) + 1];
+	char *map="0123456789ABCDEF";
+	int i=0, j=0;
+	assert(serial);
+	for(i=0; i<MLAN_SERIAL_SIZE; i++) {
+		r[j++]=map[((serial[i] & 0xf0) >> 4)];
+		r[j++]=map[(serial[i] & 0x0f)];
+	}
+	r[j]=0x00;
+	return(r);
 }
 
 /* Main */
 int
 main(int argc, char **argv)
 {
-	uchar		sn[MLAN_SERIAL_SIZE];
 	uchar		list[MAX_SERIAL_NUMS][MLAN_SERIAL_SIZE];
-	int			list_count=0, i=0, status=0, failures=0;
+	int			list_count=0, i=0, failures=0;
 	int			rslt=0;
 	const char	*sample_str=NULL;
-	char		*busdev=NULL, *curdir=NULL:
+	char		*busdev=NULL, *curdir=NULL;
 	FILE		*logfile=NULL;
 	MLan		*mlan=NULL;
 
@@ -81,7 +118,7 @@ main(int argc, char **argv)
 	assert(logfile);
 	curdir = argv[3];
 
-	if( mlan=init(busdev) == NULL ) {
+	if( (mlan=init(busdev)) == NULL ) {
 		fprintf(stderr, "Unable to initialize 9097, not a good start.\n");
 		exit(1);
 	}
@@ -102,7 +139,7 @@ main(int argc, char **argv)
 			switch(list[i][0]) {
 				case 0x10:
 					sample_str=get_sample(mlan, list[i]);
-					if(sample_str=NULL) {
+					if(sample_str==NULL) {
 						failures++;
 						sample_str="Error getting sample";
 					}
@@ -114,7 +151,21 @@ main(int argc, char **argv)
 			if(sample_str!=NULL) {
 				fprintf(logfile, "%s\t%s\t%s\n", get_time_str(),
 					get_serial(list[i]), sample_str);
+				record_cur(curdir, get_serial(list[i]), sample_str);
+				fflush(logfile);
 			}
 		}
+
+		if(failures>0) {
+			teardown(mlan);
+			fprintf(stderr, "Reinitializing\n");
+			/* Wait five seconds before reopening the device. */
+			sleep(5);
+			while( (mlan=init(busdev)) == NULL)
+				sleep(1);
+		}
+		/* Wait a while before the next sample */
+		sleep(59);
 	}
+	/* NOT REACHED */
 }

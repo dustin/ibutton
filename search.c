e5e8fdbf67825b6da2a5dcd7762d3fcaced4559c
Dustin Sallings <dustin@spy.net>
2000-07-15 15:34:07 +0000

    Added getopt and the ability to do conditional searches.
    Author: dustin
    Date: 2000/07/15 07:34:07
    
    
    Viewing status is now optional.
    
    imported from: dustin@spy.net--projects/ibutton--fromcvs--0.1--patch-37

--- search.c
+++ search.c
@@ -10,6 +10,27 @@ int main(int argc, char **argv)
 	int i, j, rslt, current=0;
 	uchar list[MAX_SERIAL_NUMS][MLAN_SERIAL_SIZE];
 	char *dev=NULL;
+	int only_alarming=FALSE;
+	int do_sample=FALSE;
+	int ch;
+
+	extern int optind;
+
+	/* Parse the arguments. */
+	while( (ch=getopt(argc, argv, "as")) != -1) {
+		switch(ch) {
+			case 'a':
+				only_alarming=TRUE;
+				break;
+			case 's':
+				do_sample=TRUE;
+			case '?':
+				break;
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
 
 	if(getenv("MLAN_DEVICE")) {
 		dev=getenv("MLAN_DEVICE");
@@ -24,14 +45,14 @@ int main(int argc, char **argv)
 		printf("Found no DS2480\n");
 	}
 
-	rslt=mlan->first(mlan, TRUE, FALSE);
+	rslt=mlan->first(mlan, TRUE, only_alarming);
 	while(rslt) {
 		mlan->copySerial(mlan, list[current++]);
-		rslt = mlan->next(mlan, TRUE, FALSE);
+		rslt = mlan->next(mlan, TRUE, only_alarming);
 	}
 
 	for(i=0; i<current; i++) {
-		char *what=NULL, *s=NULL;
+		char *what=NULL;
 		printf("Serial[%d]:  ", i);
 		for(j=0; j<MLAN_SERIAL_SIZE; j++) {
 			printf("%02X", list[i][j]);
@@ -42,8 +63,11 @@ int main(int argc, char **argv)
 		}
 		printf("\n");
 
-		if( (s=get_sample(mlan, list[i])) != NULL) {
-			printf("\tStatus reading:  %s\n", s);
+		if(do_sample) {
+			char *s=NULL;
+			if( (s=get_sample(mlan, list[i])) != NULL) {
+				printf("\tStatus reading:  %s\n", s);
+			}
 		}
 	}
 

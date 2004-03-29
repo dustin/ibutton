ed7b71245b8a13b6a35543c6823fa73e7edfc90d
Dustin Sallings <dustin@spy.net>
2004-03-29 08:26:30 +0000

    Clean up the 2406 utility a bit.
    imported from: dustin@spy.net--projects-2004/ibutton--head--1.1--patch-4

--- 2406.c
+++ 2406.c
@@ -9,20 +9,32 @@
 #include <commands.h>
 #include <ds2406.h>
 
+static void usage(char *name) {
+	fprintf(stderr, "Usage:  %s serial_number [on|off]\n"
+		"\tPrint switch status report.  Optional argument "
+		"manipulates switch A.\n", name);
+	exit(1);
+}
+
 int main(int argc, char **argv)
 {
 	MLan *mlan=NULL;
 	uchar serial[MLAN_SERIAL_SIZE];
 	char *serial_in=NULL, *dev=NULL;
 	int status=0;
-	int onoff=0;
+	int onoff=-1;
 
 	if(argc<2) {
-		fprintf(stderr, "Usage:  %s serial_number\n", argv[0]);
-		exit(1);
+		usage(argv[0]);
 	}
 	if(argc>2) {
-		onoff=atoi(argv[2]);
+		if(strcasecmp(argv[2], "on") == 0) {
+			onoff=DS2406_ON;
+		} else if(strcasecmp(argv[2], "off") == 0) {
+			onoff=DS2406_OFF;
+		} else {
+			usage(argv[0]);
+		}
 	}
 	serial_in=argv[1];
 
@@ -48,7 +60,10 @@ int main(int argc, char **argv)
 	printf("Switch A is %s\n", (status&DS2406SwitchA)?"off":"on");
 	printf("Switch B is %s\n", (status&DS2406SwitchB)?"off":"on");
 
-	setDS2406Switch(mlan, serial, DS2406SwitchA, onoff);
+	if(onoff == DS2406_OFF || onoff == DS2406_ON) {
+		printf("Turning %s %s\n", serial_in, (onoff==DS2406_ON?"on":"off"));
+		setDS2406Switch(mlan, serial, DS2406SwitchA, onoff);
+	}
 
 	mlan->destroy(mlan);
 
